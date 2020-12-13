9a0516648fb392236ca8f111efcb66fb89d5a5d7
stefanljusic10 <53339429+stefanljusic10@users.noreply.github.com>
2020-12-13 22:24:13 +0100

    views

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 let accountsTableBody = document.querySelector("#accounts-table-body");
+let links = document.querySelector(".nav-link");
+let accountsView = document.querySelector("#accounts-view");
+let addAccountsView = document.querySelector("#add-accounts-view");
+let views = document.querySelectorAll(".view");
+
+for (let i = 0; i < links.length; i++) {
+  links.addEventListener("click", showView);
+}
+
+function showView(e) {
+  e.preventDefault();
+  for (let i = 0; i < views.length; i++) {
+    views[i].style.display = "none";
+  }
+
+  let id = `#${this.getAttribute("href")}`;
+  document.querySelector(id).style.display = "block";
+}
 
 function createAccountsTable() {
   for (i = 0; i < db.length; i++) {

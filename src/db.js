c3a05059eaecc0d56f3439b15420d9c065568bd2
stefanljusic10 <53339429+stefanljusic10@users.noreply.github.com>
2020-12-14 22:45:47 +0100

    add account

--- src/db.js
+++
@@ -1,9 +0,0 @@
-let db = [
-  {
-    id: 1,
-    firstName: "Pera",
-    lastName: "Peric",
-    address: "Beograd, Branka Radicevica 12",
-    job: "postman"
-  }
-]
--- src/main.js
+++ src/main.js
@@ -1,21 +1,54 @@
+let db = [];
 let accountsTableBody = document.querySelector("#accounts-table-body");
 let links = document.querySelector(".nav-link");
 let accountsView = document.querySelector("#accounts-view");
 let addAccountsView = document.querySelector("#add-accounts-view");
 let views = document.querySelectorAll(".view");
+let firstNameInput = document.querySelector('[placeholder="First Name"]');
+let lastNameInput = document.querySelector('[placeholder="Last Name"]');
+let addressInput = document.querySelector('[placeholder="Address"]');
+let jobInput = document.querySelector('[placeholder="Job"]');
+let saveBtn = document.querySelector("#save-btn");
+
+saveBtn.addEventListener("click", saveAccount);
+
+function saveAccount(){
+  const newAccount = {
+    id: db.length + 1,
+    firstName: firstNameInput.value,
+    lastName: lastNameInput.value,
+    address: addressInput.value,
+    job: jobInput.value
+  }
+
+  db.push(newAccount);
+  firstNameInput.value = "";
+  lastNameInput.value = "";
+  addressInput.value = "";
+  jobInput.value = "";
+  createAccountsTable();
+  accountsView.style.display = "block";
+  addAccountsView.style.display = "none";
+}
+
+// Ne radi preventDefault() iz nekog razloga
+// Ne sprecava eventListener da ode na drugi link
 
 // for (let i = 0; i < links.length; i++) {
 //   links.addEventListener("click", showView);
 // }
-
 // function showView(e){
-//   e.preventDefault();
 //   for (let i = 0; i < views.length; i++) {
 //     views[i].style.display = "none";
 //   }
-
-//   let id = `#${e.target.getAttribute("href")}`;
-//   document.querySelector(id).style.display = "block";
+//   if(e instanceof Event){
+//     e.preventDefault();
+//     let id = `#${e.target.getAttribute("href")}`;
+//     document.querySelector(id).style.display = "block";
+//   }
+//   else {
+//     document.querySelector(e).style.display = "block";
+//   }
 // }
 
 document.querySelector("#add-accounts-view-btn").addEventListener("click", function(e){

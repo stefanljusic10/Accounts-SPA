da11bb51be2ca7be461e3bbfe878ad83a4d12832
stefanljusic10 <53339429+stefanljusic10@users.noreply.github.com>
2020-12-16 22:23:24 +0100

    edit account

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 let db = [];
 let accountsTableBody = document.querySelector("#accounts-table-body");
-let links = document.querySelector(".nav-link");
 let accountsView = document.querySelector("#accounts-view");
 let addAccountsView = document.querySelector("#add-accounts-view");
-let views = document.querySelectorAll(".view");
 let firstNameInput = document.querySelector('[placeholder="First Name"]');
 let lastNameInput = document.querySelector('[placeholder="Last Name"]');
 let addressInput = document.querySelector('[placeholder="Address"]');
 let jobInput = document.querySelector('[placeholder="Job"]');
 let saveBtn = document.querySelector("#save-btn");
+let editAccountsView = document.querySelector("#edit-sccounts-view");
+let editBtn = document.querySelector("#edit-btn");
 
 saveBtn.addEventListener("click", saveAccount);
 
@@ -33,7 +33,7 @@ function saveAccount() {
 
 function createAccountsTable() {
   let html = ``;
-  for (i = 0; i < db.length; i++) {
+  for (let i = 0; i < db.length; i++) {
     const account = db[i];
     html += `
     <tr>
@@ -42,25 +42,28 @@ function createAccountsTable() {
       <td>${account.lastName}</td>
       <td>${account.address}</td>
       <td>${account.job}</td>
-      <td><button id="edit${i}" class=btn btn-sm btn-warning from-control">Edit</button></td>
+      <td><button id="edit${i}" class="btn btn-sm btn-warning from-control">Edit</button></td>
       <td><button id="delete${i}" class="btn btn-sm btn-warning from-control" style="background-color: red">Delete</button></td>
     </tr>
     `
   }
   accountsTableBody.innerHTML = html;
 
-  // eventListeneri prave problem
-  for (let i = 0; i < db[i].length; i++) {
+  for (let i = 0; i < db.length; i++) {
     document.querySelector(`#edit${i}`).addEventListener("click", editAccount);
     document.querySelector(`#delete${i}`).addEventListener("click", deleteAccount);
   }
 }
 
 function editAccount(){
-
+  accountsView.style.display = "none";
+  addAccountsView.style.display = "none";
 }
-function deleteAccount(){
-
+function deleteAccount(e){
+  let idDelete = e.target.getAttribute("id");
+  idDelete = idDelete.replace("delete", "");
+  db.splice(idDelete, 1);
+  createAccountsTable();
 }
 
 createAccountsTable();
